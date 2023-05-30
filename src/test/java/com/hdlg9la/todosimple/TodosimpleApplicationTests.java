354bc1bebd5691fbaff577e28f3dbe547eb7450e
Hidélgio Novela <hidelgionovela@gmail.com>
2023-05-31 01:23:23 +0200

    Initial commit

---
+++ src/test/java/com/hdlg9la/todosimple/TodosimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hdlg9la.todosimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodosimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
