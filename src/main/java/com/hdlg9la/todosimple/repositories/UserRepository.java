43e86922def6b3a89429f5144cc8918a0dc5780d
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-05 20:32:46 +0200

    Create UserRepository.java

---
+++ src/main/java/com/hdlg9la/todosimple/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.hdlg9la.todosimple.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.hdlg9la.todosimple.models.User;
+// import java.util.List;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+   
+}
