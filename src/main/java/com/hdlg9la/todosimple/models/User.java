b08c840f8bfa6b77384e295a6a4800b2585db819
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-05 19:55:55 +0200

    Update user model

--- src/main/java/com/hdlg9la/todosimple/models/User.java
+++ src/main/java/com/hdlg9la/todosimple/models/User.java
@@ -1,18 +1,33 @@
 package com.hdlg9la.todosimple.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "user")
 public class User {
+
+         public interface CreateUser {
+
+         }
+
+         public interface UpdateUser {
+
+         }
 
          @Id
          @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,16 +35,107 @@ public class User {
          private Long id;
 
          @Column(name = "username", length = 100, nullable = false, unique = true)
-         @NotNull
-         @NotEmpty
-         @Size(min = 2, max = 50)
+         @NotNull(groups = CreateUser.class)
+         @NotEmpty(groups = CreateUser.class)
+         @Size(groups = CreateUser.class, min = 2, max = 50)
          private String username;
 
-         @Column(name = "username", length = 60, nullable = false)
-         @NotNull
-         @NotEmpty
-         @Size(min = 4, max = 8)
+
+         @JsonProperty(access = Access.WRITE_ONLY)
+         @Column(name = "password", length = 60, nullable = false)
+         @NotNull(groups = { CreateUser.class, UpdateUser.class })
+         @NotEmpty(groups = { CreateUser.class, UpdateUser.class })
+         @Size(groups = { CreateUser.class, UpdateUser.class }, min = 4, max = 8)
          private String password;
 
+
+         @OneToMany(mappedBy = "user")
+         private List<Task> tasks = new ArrayList<Task>();
+
+         public User() {
+
+         }
+
+         public User(Long id,
+                           @NotNull(groups = CreateUser.class) @NotEmpty(groups = CreateUser.class) @Size(groups = CreateUser.class, min = 2, max = 50) String username,
+                           @NotNull(groups = { CreateUser.class, UpdateUser.class }) @NotEmpty(groups = {
+                                             CreateUser.class, UpdateUser.class }) @Size(groups = { CreateUser.class,
+                                                               UpdateUser.class }, min = 4, max = 8) String password) {
+                  this.id = id;
+                  this.username = username;
+                  this.password = password;
+         }
+
+
+         
+
+         public Long getId() {
+                  return id;
+         }
+
+         public void setId(Long id) {
+                  this.id = id;
+         }
+
+         public String getUsername() {
+                  return username;
+         }
+
+         public void setUsername(String username) {
+                  this.username = username;
+         }
+
+         public String getPassword() {
+                  return password;
+         }
+
+         public void setPassword(String password) {
+                  this.password = password;
+         }
+
+         @Override
+         public int hashCode() {
+                  final int prime = 31;
+                  int result = 1;
+                  result = prime * result + ((id == null) ? 0 : id.hashCode());
+                  result = prime * result + ((username == null) ? 0 : username.hashCode());
+                  result = prime * result + ((password == null) ? 0 : password.hashCode());
+                  return result;
+         }
+
+         @Override
+         public boolean equals(Object obj) {
+                  if (this == obj)
+                           return true;
+                  if (obj == null)
+                           return false;
+                  if (getClass() != obj.getClass())
+                           return false;
+                  User other = (User) obj;
+                  if (id == null) {
+                           if (other.id != null)
+                                    return false;
+                  } else if (!id.equals(other.id))
+                           return false;
+                  if (username == null) {
+                           if (other.username != null)
+                                    return false;
+                  } else if (!username.equals(other.username))
+                           return false;
+                  if (password == null) {
+                           if (other.password != null)
+                                    return false;
+                  } else if (!password.equals(other.password))
+                           return false;
+                  return true;
+         }
+
+         public List<Task> getTasks() {
+                  return tasks;
+         }
+
+         public void setTasks(List<Task> tasks) {
+                  this.tasks = tasks;
+         }
+
 }
-// Paramos no minuto 18
