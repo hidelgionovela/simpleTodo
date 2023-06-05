c8dee2fb764019320ae67d93e8ff4001694a0419
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-05 22:01:20 +0200

    Create TaskService.java

---
+++ src/main/java/com/hdlg9la/todosimple/services/TaskService.java
@@ -0,0 +1,71 @@
+package com.hdlg9la.todosimple.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hdlg9la.todosimple.models.Task;
+import com.hdlg9la.todosimple.models.User;
+import com.hdlg9la.todosimple.repositories.TaskRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class TaskService {
+
+         @Autowired
+         private UserService userService;
+
+         @Autowired
+         private TaskRepository taskRepository;
+
+
+
+         public Task findById(Long id){
+                
+                  Optional<Task> task = this.taskRepository.findById(id) ;
+
+                  return task.orElseThrow(() -> new RuntimeException(
+                           "Tarefa nao encontrada! id: " +id+ ", Tipo: " + Task.class.getName() ));
+         }
+
+         @Transactional
+         public Task create (Task task){
+                  User user = this.userService.findById(task.getUser().getId());
+                  task.setId(null);
+                  task.setUser(user);
+                  task = this.taskRepository.save(task);
+                  return task;
+
+         }
+
+         @Transactional
+         public Task update (Task task){
+                  
+                  Task newTask = findById(task.getId());
+                  newTask.setDescription(task.getDescription());
+                  return this.taskRepository.save(newTask);
+
+         }
+
+         // @Transactional
+         public void delete (Long id){
+                  
+                  findById(id);
+
+                  try {
+                           this.taskRepository.deleteById(id);
+                  } catch (Exception e) {
+            
+                           throw new RuntimeException("Nao é possivel excluir pois ha entidades relacionadas");
+                  }
+
+         }
+
+         
+
+         
+
+         
+}
