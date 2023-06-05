6ff99e395ce5d4c23798f217df9d3db63c8af134
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-05 22:01:17 +0200

    Update TaskRepository.java

--- src/main/java/com/hdlg9la/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/hdlg9la/todosimple/repositories/TaskRepository.java
@@ -1,8 +1,8 @@
 package com.hdlg9la.todosimple.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+// import org.springframework.data.jpa.repository.Query;
+// import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.hdlg9la.todosimple.models.Task;
