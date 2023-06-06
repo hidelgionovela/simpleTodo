0ab2555759ffd4a84dcde37ff750ace118106d94
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-06 19:22:32 +0200

    Update UserService.java

--- src/main/java/com/hdlg9la/todosimple/services/UserService.java
+++ src/main/java/com/hdlg9la/todosimple/services/UserService.java
@@ -1,5 +1,6 @@
 package com.hdlg9la.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,20 +17,24 @@ public class UserService {
          @Autowired
          private UserRepository userRepository;
 
-
-
-         public User findById(Long id){
-                  // O optional é uma classe do java util que impede o lancamento de 
-                  // da excessao nullpointer exception em caso de se retornal null entao caso 
-                  // nao se encontre os dados retorna vazio.  
-                  Optional<User> user = this.userRepository.findById(id) ;
+         public User findById(Long id) {
+                  // O optional é uma classe do java util que impede o lancamento de
+                  // da excessao nullpointer exception em caso de se retornal null entao caso
+                  // nao se encontre os dados retorna vazio.
+                  Optional<User> user = this.userRepository.findById(id);
 
                   return user.orElseThrow(() -> new RuntimeException(
-                           "Usuario nao encontrado! id: " +id+ ", Tipo: " + User.class.getName() ));
+                                    "Usuario nao encontrado! id: " + id + ", Tipo: " + User.class.getName()));
          }
 
+
+         public List <User> getAllUsers() {
+                  return userRepository.findAll();
+         }
+         
+
          @Transactional
-         public User create (User user){
+         public User create(User user) {
                   // recomendado usar o trasational em funcoes que alteram algo na base de dados
                   user.setId(null);
                   user = this.userRepository.save(user);
@@ -38,31 +43,27 @@ public class UserService {
          }
 
          @Transactional
-         public User update (User user){
-                  
+         public User update(User user) {
+
                   User newUser = findById(user.getId());
                   newUser.setPassword(user.getPassword());
+                  newUser.setUsername(user.getUsername());
                   return this.userRepository.save(newUser);
 
          }
 
          // @Transactional
-         public void delete (Long id){
-                  
+         public void delete(Long id) {
+
                   findById(id);
 
                   try {
                            this.userRepository.deleteById(id);
                   } catch (Exception e) {
-            
+
                            throw new RuntimeException("Nao é possivell excluir pois ha entidades relacionadas");
                   }
 
          }
 
-         
-
-         
-
-         
 }
