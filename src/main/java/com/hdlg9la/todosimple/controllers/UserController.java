d70482b26e9fef66ca333cc0fe27edb7bae78327
Hidélgio Novela <hidelgionovela@gmail.com>
2023-06-12 22:15:10 +0200

    Update UserController.java

--- src/main/java/com/hdlg9la/todosimple/controllers/UserController.java
+++ src/main/java/com/hdlg9la/todosimple/controllers/UserController.java
@@ -27,48 +27,48 @@ import jakarta.validation.Valid;
 @RequestMapping("/user")
 @Validated
 public class UserController {
-    
-         @Autowired
-         private UserService userService;
 
-         @GetMapping("/{id}")
-         public ResponseEntity<User> findUserById(@PathVariable Long id){
+        @Autowired
+        private UserService userService;
 
-                  User user = this.userService.findById(id);
-                  return ResponseEntity.ok().body(user);
+        @GetMapping("/{id}")
+        public ResponseEntity<User> findUserById(@PathVariable Long id) {
 
-         }
+                User user = this.userService.findById(id);
+                return ResponseEntity.ok().body(user);
 
-         @GetMapping
-         public ResponseEntity<List<User>> findAllUsers(){
+        }
 
-                  List <User> user = this.userService.getAllUsers();
-                  return ResponseEntity.ok().body(user);
+        @GetMapping
+        public ResponseEntity<List<User>> findAllUsers() {
 
-         }
+                List<User> user = this.userService.getAllUsers();
+                return ResponseEntity.ok().body(user);
 
-         @PostMapping
-         @Validated(CreateUser.class)
-         public ResponseEntity<Void> create (@Valid @RequestBody User user){
-                  this.userService.create(user);
-                  URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
-                  return ResponseEntity.created(uri).build();
-         }
+        }
 
-         @PutMapping("/{id}")
-         @Validated(UpdateUser.class)
-         public ResponseEntity <Void> update(@Valid @RequestBody User user, @PathVariable Long id){
-                 user.setId(id);
-                 this.userService.update(user);
-                 return ResponseEntity.noContent().build(); 
-         }
+        @PostMapping
+        @Validated(CreateUser.class)
+        public ResponseEntity<Void> create(@Valid @RequestBody User user) {
+                this.userService.create(user);
+                URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId())
+                                .toUri();
+                return ResponseEntity.created(uri).build();
+        }
 
-         @DeleteMapping("/{id}")
-         public ResponseEntity <Void> delete(@PathVariable Long id){
-                  this.userService.delete(id);
-                  return ResponseEntity.noContent().build();
+        @PutMapping("/{id}")
+        @Validated(UpdateUser.class)
+        public ResponseEntity<Void> update(@Valid @RequestBody User user, @PathVariable Long id) {
+                user.setId(id);
+                this.userService.update(user);
+                return ResponseEntity.noContent().build();
+        }
 
-         }
+        @DeleteMapping("/{id}")
+        public ResponseEntity<Void> delete(@PathVariable Long id) {
+                this.userService.delete(id);
+                return ResponseEntity.noContent().build();
 
+        }
 
 }
