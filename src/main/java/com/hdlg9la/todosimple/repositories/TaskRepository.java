8eaae67cb584d61b8146c5824e4213c21379cbd9
hidelgio9La <89780156+hidelgionovela@users.noreply.github.com>
2023-06-28 22:32:16 +0200

    Update TaskRepository.java

--- src/main/java/com/hdlg9la/todosimple/repositories/TaskRepository.java
+++ src/main/java/com/hdlg9la/todosimple/repositories/TaskRepository.java
@@ -3,12 +3,12 @@ package com.hdlg9la.todosimple.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 // import org.springframework.data.jpa.repository.Query;
 // import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+// import org.springframework.stereotype.Repository;
 
 import com.hdlg9la.todosimple.models.Task;
 import java.util.List;
 
-@Repository
+// @Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
          List<Task> findByUser_Id(Long id);
