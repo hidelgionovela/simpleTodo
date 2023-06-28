736265a17328c2f4bdc3e29c6f3e2e64dac88588
hidelgio9La <89780156+hidelgionovela@users.noreply.github.com>
2023-06-28 22:32:04 +0200

    Create script.js

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,8 @@
+// Arquivo que faz a conexao com a API
+
+const url = "http://localhost:8080/task/1"
+
+function hideLoader() {
+         document.getElementById("loading").style.display = "none";
+         
+}
