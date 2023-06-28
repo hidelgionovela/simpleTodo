e1aab79d7e8a5c79ab8d62beea808d871c0d5f19
hidelgio9La <89780156+hidelgionovela@users.noreply.github.com>
2023-06-29 00:35:40 +0200

    Delete script.js

--- src/main/resources/static/script.js
+++
@@ -1,8 +0,0 @@
-// Arquivo que faz a conexao com a API
-
-const url = "http://localhost:8080/task/1"
-
-function hideLoader() {
-         document.getElementById("loading").style.display = "none";
-         
-}
