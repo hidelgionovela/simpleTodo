29e44f3aa0adbd5d89216ba1cec86c143e7ed4a8
hidelgio9La <89780156+hidelgionovela@users.noreply.github.com>
2023-06-30 22:24:22 +0200

    Update WebConfigCors.java

--- src/main/java/com/hdlg9la/todosimple/config/WebConfigCors.java
+++ src/main/java/com/hdlg9la/todosimple/config/WebConfigCors.java
@@ -14,6 +14,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfigCors implements WebMvcConfigurer {
      
          public void addCorsMappings(CorsRegistry registry) {
-                  registry.addMapping("/**");
+                  registry.addMapping("/**"); 
          }
 }
